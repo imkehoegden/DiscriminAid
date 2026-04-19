d4968fe55dbeb7a2dd39cbfe640b541b5e632feb
imkehoegden <imke.hoegden@posteo.de>
2026-04-19 18:29:09 +0200

    Style: InfoBlock Pfeil angepasst

--- discriminaid/app/components/InfoBlock.tsx
+++ discriminaid/app/components/InfoBlock.tsx
@@ -66,7 +66,7 @@ export default function InfoBlock({
             href={link.href}
             className={`inline-block mt-4 underline underline-offset-4 hover:text-[var(--secondarytext)] transition ${textMap[textColor]}`}
           >
-            {link.text} →
+            {link.text} <span className="text-xl">›</span>
           </Link>
         )}
       </div>
