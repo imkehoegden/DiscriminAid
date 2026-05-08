f136c2c43de0bc816a2ef030c6f759ee91546b0f
imkehoegden <imke.hoegden@posteo.de>
2026-05-08 13:29:52 +0200

    Refactor: remove unused component

--- app/components/VideoSection.tsx
+++
@@ -1,7 +0,0 @@
-export function VideoSection({ children }: { children: React.ReactNode }) {
-  return (
-    <section className="max-w-6xl px-12 py-16 mx-auto">
-      {children}
-    </section>
-  );
-}
